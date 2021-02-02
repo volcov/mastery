9994dbfd501f6dc1c10e2a1df27062a626601ac8
volcov <volcov.bruno@gmail.com>
2021-02-02 01:11:59 -0300

    quiz has templates

--- lib/mastery/core/quiz.ex
+++ lib/mastery/core/quiz.ex
@@ -1,4 +1,6 @@
 defmodule Mastery.Core.Quiz do
+  alias Mastery.Core.{Template, Question, Response}
+
   defstruct title: nil,
             mastery: 3,
             templates: %{},
@@ -7,4 +9,24 @@ defmodule Mastery.Core.Quiz do
             last_response: nil,
             record: %{},
             mastered: []
+
+  def new(fields) do
+    struct!(__MODULE__, fields)
+  end
+
+  def add_template(quiz, fields) do
+    template = Template.new(fields)
+
+    templates =
+      update_in(
+        quiz.templates,
+        [template.category],
+        &add_to_list_or_nil(&1, template)
+      )
+
+    %{quiz | templates: templates}
+  end
+
+  defp add_to_list_or_nil(nil, template), do: [template]
+  defp add_to_list_or_nil(templates, template), do: [template | templates]
 end
