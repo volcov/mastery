84a646f504e2159ba81342dc8c7aa85003559dda
volcov <volcov.bruno@gmail.com>
2021-02-02 01:31:16 -0300

    templates actions on quiz

--- lib/mastery/core/quiz.ex
+++ lib/mastery/core/quiz.ex
@@ -27,6 +27,53 @@ defmodule Mastery.Core.Quiz do
     %{quiz | templates: templates}
   end
 
+  def select_question(%__MODULE__{templates: t}) when map_size(t) == 0, do: nil
+
+  def select_question(quiz) do
+    quiz
+    |> pick_current_question()
+    |> move_template(:used)
+    |> reset_template_cycle()
+  end
+
   defp add_to_list_or_nil(nil, template), do: [template]
   defp add_to_list_or_nil(templates, template), do: [template | templates]
+
+  defp pick_current_question(quiz) do
+    Map.put(quiz, :current_question, select_a_random_question(quiz))
+  end
+
+  defp select_a_random_question(quiz) do
+    quiz.templates
+    |> Enum.random()
+    |> elem(1)
+    |> Enum.random()
+    |> Question.new()
+  end
+
+  defp move_template(quiz, field) do
+    quiz
+    |> remove_template_from_category()
+    |> add_template_to_field(field)
+  end
+
+  defp template(quiz), do: quiz.current_question.template
+
+  defp remove_template_from_category(quiz) do
+    template = template(quiz)
+
+    new_category_templates =
+      quiz.templates
+      |> Map.fetch!(template.category)
+      |> List.delete(template)
+
+    new_templates =
+      if new_category_templates == [] do
+        Map.delete(quiz.templates, template.category)
+      else
+        Map.put(quiz.templates, template.category, new_category_templates)
+      end
+
+    Map.put(quiz, :templates, new_templates)
+  end
 end
